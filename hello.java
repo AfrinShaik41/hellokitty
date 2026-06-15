52c6222ee83bdfa1d62d06de8bd2d549a25b9f36
AfrinShaik41 <afrin.appu.27@gmail.com>
2026-06-15 13:10:58 +0530

    Add sum calculation to Main class

--- hello.java
+++ hello.java
@@ -3,3 +3,11 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
