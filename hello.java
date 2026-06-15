5cb802764424ea7d1c83e8232891cdb85d3f5297
root <root@ip-172-31-17-105.ec2.internal>
2026-06-15 07:46:57 +0000

    done

--- hello.java
+++ hello.java
@@ -1,8 +1,11 @@
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
 }
+
 public class Main {
     public static void main(String[] args) {
         int a = 10;
@@ -11,3 +14,6 @@ public class Main {
         System.out.println("Sum = " + (a + b));
     }
 }
+
+ this is my code in java
+
